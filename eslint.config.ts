0deb53592f1e67e93bfb07e9ff56916ed8f643d5
antruc <admin@antruc.dev>
2026-05-16 14:29:34 -0600

    Fix ESLint and TypeScript integration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,19 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import prettier from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
 
 export default [
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   prettier,
   {
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
-        ...globals.browser
+        ...globals.browser,
+        ...globals.node
       }
     }
   }
